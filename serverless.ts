ef07d4174b9fff8c6bdbfd507cf14171bffd79c3
Tiago Muniz de Araujo <102497603+TiagoAraujoDev@users.noreply.github.com>
2022-10-06 10:26:54 -0300

    deploy

--- serverless.ts
+++ serverless.ts
@@ -28,7 +28,7 @@ const serverlessConfiguration: AWS = {
       },
     ],
   },
-  // import the function via paths
+  package: { individually: false, include: ["./src/templates/**"] },
   functions: {
     generateCertificate: {
       handler: "src/functions/generateCertificate.handler",
@@ -42,8 +42,19 @@ const serverlessConfiguration: AWS = {
         },
       ],
     },
+    verifyCertificate: {
+      handler: "src/functions/verifyCertificate.handler",
+      events: [
+        {
+          http: {
+            path: "verifyCertificate/{id}",
+            method: "get",
+            cors: true,
+          },
+        },
+      ],
+    },
   },
-  package: { individually: true },
   custom: {
     esbuild: {
       bundle: true,
--- src/functions/generateCertificate.ts
+++ src/functions/generateCertificate.ts
@@ -3,6 +3,7 @@ import { document } from "../utils/dynamodbClient";
 import { compile } from "handlebars";
 import dayjs from "dayjs";
 import chromium from "chrome-aws-lambda";
+import { S3 } from "aws-sdk";
 
 import { join } from "path";
 import { readFileSync } from "fs";
@@ -32,18 +33,6 @@ const compileTemplate = async (data: ITemplate) => {
 export const handler: APIGatewayProxyHandler = async (event) => {
   const { id, name, grade } = JSON.parse(event.body) as ICreateCertificate;
 
-  await document
-    .put({
-      TableName: "users_certificate",
-      Item: {
-        id,
-        name,
-        grade,
-        created_at: new Date().getTime(),
-      },
-    })
-    .promise();
-
   const response = await document
     .query({
       TableName: "users_certificate",
@@ -53,6 +42,22 @@ export const handler: APIGatewayProxyHandler = async (event) => {
       },
     })
     .promise();
+
+  const userAlreadyExist = response.Items[0];
+
+  if (!userAlreadyExist) {
+    await document
+      .put({
+        TableName: "users_certificate",
+        Item: {
+          id,
+          name,
+          grade,
+          created_at: new Date().getTime(),
+        },
+      })
+      .promise();
+  }
 
   const medalPath = join(process.cwd(), "src", "templates", "selo.png");
   const medal = readFileSync(medalPath, "base64");
@@ -87,8 +92,23 @@ export const handler: APIGatewayProxyHandler = async (event) => {
 
   await browser.close();
 
+  const s3 = new S3();
+
+  await s3
+    .putObject({
+      Bucket: "certificateignite42",
+      Key: `${id}.pdf`,
+      ACL: "public-read",
+      Body: pdf,
+      ContentType: "application/pdf",
+    })
+    .promise();
+
   return {
     statusCode: 201,
-    body: JSON.stringify(response.Items[0]),
+    body: JSON.stringify({
+      message: "Certificado criado com sucesso!",
+      url: `https://certificateignite42.s3.amazonaws.com/${id}.pdf`,
+    }),
   };
 };
